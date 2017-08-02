4fbf672b9d8b198e0729e1faba150ddc68395438
Ori Harel <ori.harel@gmail.com>
2017-08-02 17:03:56 +0300

    more work

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 let RNRandomBytes = require('react-native').NativeModules.RNRandomBytes;
+
+export function randomBytes(length, cb) {
+
+    RNRandomBytes.randomBytes(length, function(err, base64String) {
+        if (err) {
+            cb(err)
+        } else {
+            cb(null, toBuffer(base64String))
+        }
+    })
+}
