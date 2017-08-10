c5a6394d041d07b2da168a5dfe3148da23e76446
Ori Harel <ori.harel@gmail.com>
2017-08-10 11:53:14 +0300

    more work

--- android/src/main/java/com/capriza/randombytes/RandomBytesPackage.java
+++ android/src/main/java/com/capriza/randombytes/RandomBytesPackage.java
@@ -1,5 +1,7 @@
 package com.capriza.randombytes;
 
+import android.util.Log;
+
 import com.facebook.react.ReactPackage;
 import com.facebook.react.bridge.JavaScriptModule;
 import com.facebook.react.bridge.NativeModule;
@@ -11,10 +13,13 @@ import java.util.Collections;
 import java.util.List;
 
 public class RandomBytesPackage implements ReactPackage {
+  public static final String LOG_TAG = RandomBytesPackage.class.getSimpleName();
 
   @Override
   public List<NativeModule> createNativeModules(
                               ReactApplicationContext reactContext) {
+    Log.d(LOG_TAG, "createNativeModules started");
+
     List<NativeModule> modules = new ArrayList<>();
 
     modules.add(new RandomBytesModule(reactContext));
@@ -24,11 +29,14 @@ public class RandomBytesPackage implements ReactPackage {
 
   @Override
   public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
+    Log.d(LOG_TAG, "createViewManagers started");
+
     return Collections.emptyList();
   }
 
   @Override
   public List<Class<? extends JavaScriptModule>> createJSModules() {
+    Log.d(LOG_TAG, "createJSModules started");
     return Collections.emptyList();
   }
 }
--- index.js
+++ index.js
@@ -1,12 +1,27 @@
 let RNRandomBytes = require('react-native').NativeModules.RNRandomBytes;
+let _isInitialized = false, _randomBytesStrings = [], _length;
 
-export function randomBytes(length, cb) {
-
-    RNRandomBytes.randomBytes(length, function(err, base64String) {
-        if (err) {
-            cb(err)
-        } else {
-            cb(null, toBuffer(base64String))
-        }
+function addRandomBytesString(){
+    RNRandomBytes.randomBytes(_length).then((randomBytesString)=>{
+        console.log('pushing another random string');
+        _randomBytesStrings.push(randomBytesString);
     })
+}
+
+export async function init(length) {
+    console.log('initializing RNRandomBytes');
+    _length = length;
+    for (let i = 0; i < 10; i++) {
+        let randomBytesString = await RNRandomBytes.randomBytes(length)
+        _randomBytesStrings.push(randomBytesString);
+    }
+    console.log(`RNRandomBytes is initialized with ${_randomBytesStrings.length} strings`);
+}
+
+export function randomBytes(arr) {
+    console.log(`randomBytes called. Module has ${_randomBytesStrings.length} ready`);
+    let nextRandomByteString = _randomBytesStrings.pop();
+    addRandomBytesString();
+    arr = nextRandomByteString.split('');
+    console.log('randomBytes arr is: '+arr);
 }
