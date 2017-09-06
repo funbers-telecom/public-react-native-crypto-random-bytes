6f620cafd6f9eb7e4a0b5aa237aaed3c0b7ef1d6
Guy Blank <guy.blank@capriza.com>
2017-09-06 11:40:07 +0300

    import issue reverse logic

--- ios/RNRandomBytes.h
+++ ios/RNRandomBytes.h
@@ -1,11 +1,11 @@
 
 #import <Foundation/Foundation.h>
-#if __has_include("RCTBridgeModule.h")
-#import "RCTBridgeModule.h"
-#import "RCTBridge.h"
-#else
+#if __has_include(<React/RCTBridgeModule.h>)
 #import <React/RCTBridgeModule.h>
 #import <React/RCTBridge.h>
+#else
+#import "RCTBridgeModule.h"
+#import "RCTBridge.h"
 #endif
 
 @interface RNRandomBytes : NSObject <RCTBridgeModule>
